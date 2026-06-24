55675c8fa03a99d4e638517e40dda05ea31f2f3b
JAYANTI-THAKOR-941 <jyantithakor941@gmail.com>
2026-06-24 10:44:10 +0530

    curd operation

--- bookStore/controllers/productControllers.js
+++ bookStore/controllers/productControllers.js
@@ -14,3 +14,23 @@ export const createProduct = async (req, res) => {
   res.status(201).json({message:"Product Added Successfully.!"})
 
 };
+
+export const updateProduct = async(req,res)=>{
+      await Product.findByIdAndUpdate(req.params.id,req.body,{new:true});
+      res.status(200).json({message:"Product updated successfully.!"})
+}
+
+export const deleteProduct = async(req,res)=>{
+  await Product.findByIdAndDelete(req.params.id);
+  res.status(200).json({message:"Product delete successfully.!"})
+}
+
+export const getAllProducts = async(req,res)=>{
+  const products = await Product.find();
+  res.status(200).json(products);
+}
+
+export const getProductById = async(req,res)=>{
+  const product = await Product.findById(req.params.id);
+  res.status(200).json(product);
+}
--- bookStore/routes/productRoutes.js
+++ bookStore/routes/productRoutes.js
@@ -1,8 +1,18 @@
-import express from 'express';
-import { createProduct } from '../controllers/productControllers.js';
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
-router.post('/create',createProduct);
+router.post("/create", createProduct);
+router.put("/update/:id", updateProduct);
+router.delete("/delete/:id", deleteProduct);
+router.get("/all", getAllProducts);
+router.get("/:id", getProductById);
 
 export default router;
